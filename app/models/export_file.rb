1b8cb6a2ce507d845bcfcad06011ec9addb140f6
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-06-01 21:17:26 +0900

    updated annotations

--- app/models/export_file.rb
+++ app/models/export_file.rb
@@ -27,3 +27,18 @@ class ExportFile < ActiveRecord::Base
     end
   end
 end
+
+# == Schema Information
+#
+# Table name: export_files
+#
+#  id                  :integer          not null, primary key
+#  export_file_name    :string(255)
+#  export_content_type :string(255)
+#  export_file_size    :string(255)
+#  state               :string(255)
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  user_id             :integer
+#
+
--- spec/models/export_file_spec.rb
+++ spec/models/export_file_spec.rb
@@ -3,3 +3,18 @@ require 'spec_helper'
 describe ExportFile do
 #  pending "add some examples to (or delete) #{__FILE__}"
 end
+
+# == Schema Information
+#
+# Table name: export_files
+#
+#  id                  :integer          not null, primary key
+#  export_file_name    :string(255)
+#  export_content_type :string(255)
+#  export_file_size    :string(255)
+#  state               :string(255)
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  user_id             :integer
+#
+
